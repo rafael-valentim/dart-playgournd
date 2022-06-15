7e84eefc4c2dcd035553379096e2eb5ff91a5732
Rafael Valentim <rafaelvalentim.developer@gmail.com>
2022-06-15 15:52:03 -0400

    Add Object

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,3 +1,14 @@
 void main() {
-  print('Nova branch criada');
+  var dog = Dog();
+  dog.name = 'Thor';
+  dog.type = 'cachorro';
+  print('${dog.name} e ${dog.type}');
+}
+
+class Animal {
+  String name = 'teste';
+}
+
+class Dog extends Animal {
+  late String type;
 }
