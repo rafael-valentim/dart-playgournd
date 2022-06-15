def2287e1080d6c9b426cf35dc5a93cfa4337516
Rafael Valentim <rafaelvalentim.developer@gmail.com>
2022-06-15 16:41:07 -0400

    Adicionado outro objeto

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,8 +1,11 @@
 void main() {
-  var dog = Dog();
+  Dog dog = Dog();
   dog.name = 'Thor';
   dog.type = 'cachorro';
   print('${dog.name} e ${dog.type}');
+  Dog dog2 = Dog();
+  dog2.name = 'Jack';
+  dog2.type = 'cachorro';
 }
 
 class Animal {
