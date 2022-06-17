cb71eb7277d78dbb9d9ad430f5fc6927a454264a
Rafael Valentim <rafaelvalentim.developer@gmail.com>
2022-06-17 16:35:57 -0400

    testando herança de classe

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,3 +1,15 @@
 void main() {
-  print('Nova branch criada');
+  Funcionario funcionario = Funcionario();
+  funcionario.nome = 'Rafael Valentim';
+  funcionario.cargo = 'Programador';
+  print('Nome: ${funcionario.nome} - Cargo: ${funcionario.cargo}');
+}
+
+class Pessoa {
+  String nome = '';
+  Pessoa({this.nome = 'Pedro'});
+}
+
+class Funcionario extends Pessoa {
+  String cargo = '';
 }
